cc54c297c15555da0e1fb55fbbbfdc766df02340
droid-janderson <jandersongit@gmail.com>
2024-01-29 17:41:54 -0300

    inputs pages add

---
+++ pages/franquias/_id/entradas/index.vue
@@ -0,0 +1,229 @@
+<!-- eslint-disable-next-line vue/no-v-html -->
+<template>
+  <v-container>
+    <v-app-bar color="tertiary" class="mb-2 font-weight-bold align-center">
+      <v-toolbar-title
+        class="text-capitalize text-h6 font-weight-medium"
+        :style="{ color: $vuetify.theme.currentTheme.primary }"
+      >
+        <v-icon color="primary" dark class="mr-1">mdi-cart-plus</v-icon>
+        Entradas
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" @click="toPush"
+        ><v-icon>mdi-plus</v-icon> Nova Entrada</v-btn
+      >
+    </v-app-bar>
+
+    <!-- Simple table -->
+    <v-simple-table
+      fixed-header
+      height="75vh"
+      :style="{
+        backgroundColor: $vuetify.theme.currentTheme.tertiary,
+        color: $vuetify.theme.currentTheme.text_primary,
+      }"
+      class="elevation-8"
+    >
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <template v-for="item in headers">
+              <th
+                :key="item.text"
+                :style="{
+                  backgroundColor: $vuetify.theme.currentTheme.tertiary,
+                  color: $vuetify.theme.currentTheme.text_tertiary,
+                }"
+                :class="item.center ? 'text-center' : 'text-left'"
+              >
+                {{ item.text }}
+              </th>
+            </template>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in productsData" :key="item.name">
+            <td>{{ item.stockId }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.category }}</td>
+            <td class="text-center">{{ item.unitPrice }}</td>
+            <td class="text-center">{{ item.stockValue }}</td>
+            <td>{{ item.unitType }}</td>
+            <td class="text-center">{{ item.stockQuantity }}</td>
+            <td class="text-center">{{ item.minimumQuantity }}</td>
+            <td class="text-center">{{ item.discontinued ? "Sim" : "Não" }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+
+    <!-- Modal add produto -->
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card
+          :style="{ backgroundColor: $vuetify.theme.currentTheme.tertiary }"
+        >
+          <v-card-title>
+            <span class="text-h5">Adicionar produto</span>
+          </v-card-title>
+          <v-card-text>
+            <v-form v-model="validate.valid">
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-autocomplete
+                      :items="productsData"
+                      item-text="name"
+                      return-object
+                      v-model="product"
+                      background-color="background"
+                      outlined
+                      label="Produto"
+                      clearable
+                      hide-details
+                      class="elevation-1"
+                      required
+                    ></v-autocomplete>
+
+                    {{ product }}
+                  </v-col>
+                </v-row>
+                <v-row v-if="product?.perishable">
+                  <v-col cols="6">
+                    <v-menu
+                      v-model="active"
+                      :close-on-content-click="false"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="260px"
+                    >
+                      <template v-slot:activator="{ on }">
+                        <v-text-field
+                          v-model="productPerishable.expirationDate"
+                          label="Data de vencimento"
+                          outlined
+                          readonly
+                          hide-details
+                          class="elevation-1"
+                          v-on="on"
+                          @click:append="active = true"
+                        >
+                          <template v-slot:append>
+                            <v-icon>mdi-calendar</v-icon>
+                          </template>
+                        </v-text-field>
+                      </template>
+                      <v-date-picker
+                        v-model="productPerishable.expirationDate"
+                        color="backgroundNav"
+                        @input="active = false"
+                      ></v-date-picker>
+                    </v-menu>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="secondary" text @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+              Fechar
+            </v-btn>
+            <v-btn color="primary" text @click="addProduct"> Salvar </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "Products",
+  data() {
+    return {
+      dialog: false,
+      active: false,
+      headers: [
+        {
+          text: "ID de estoque",
+          value: "stockId",
+          center: false,
+        },
+        {
+          text: "Nome",
+          value: "name",
+          center: false,
+        },
+        { text: "Categoria", value: "category", center: false },
+        { text: "Preço unitário", value: "unitPrice", center: true },
+        { text: "Valor do estoque", value: "stockValue", center: true },
+        { text: "Unidade", value: "unitType", center: false },
+        { text: "Quantidade", value: "stockQuantity", center: true },
+        { text: "Quantidade Mínima", value: "minimumQuantity", center: true },
+        { text: "Descontinuado?", value: "discontinued", center: true },
+      ],
+      itemsSelect: ["Unidade", "Quilogramas", "Litros", "Centímetro", "Caixa"],
+      validate: {
+        valid: false,
+        firstname: "",
+        lastname: "",
+        nameRules: [
+          (v) => !!v || "Name is required",
+          (v) => v.length <= 10 || "Name must be less than 10 characters",
+        ],
+        email: "",
+        emailRules: [
+          (v) => !!v || "E-mail is required",
+          (v) => /.+@.+/.test(v) || "E-mail must be valid",
+        ],
+      },
+      productPerishable: {
+        expirationDate: null,
+      },
+      product: null,
+    };
+  },
+
+  computed: {
+    ...mapGetters("products", ["productsData"]),
+    ...mapGetters("franchises", ["franchiseId"]),
+  },
+
+  async created() {
+    await this.getProducts();
+  },
+
+  methods: {
+    ...mapActions("products", ["getProducts", "saveProduct"]),
+    async addProduct() {
+      try {
+        await this.saveProduct(this.product);
+        this.product = {
+          stockId: "",
+          name: "",
+          category: "",
+          unitPrice: "",
+          stockValue: "",
+          unitType: "",
+          perishable: false,
+          stockQuantity: 0,
+          minimumQuantity: 0,
+          discontinued: false,
+        };
+
+        this.dialog = false;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    async toPush() {
+      this.$router.push(`/franquias/${this.franchiseId}/entradas/lancamento`);
+    },
+  },
+};
+</script>
---
+++ store/inputs.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-useless-catch */
+export const state = () => ({
+  products: null,
+  productData: null,
+});
+
+export const mutations = {
+  setProductData(state, data) {
+    state.productData = data;
+  },
+  setProducts(state, payload) {
+    state.products = payload;
+  },
+};
+
+export const actions = {
+  async getProducts({ commit, rootState }) {
+    try {
+      const franchiseId = rootState.franchises.idFranchise;
+      const db = this.$fire.firestore;
+      const franchiseRef = db
+        .collection("users")
+        .doc(rootState.auth.user.uid)
+        .collection("franchises")
+        .doc(franchiseId)
+        .collection("products");
+
+      franchiseRef.onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        commit("setProducts", data);
+      });
+    } catch (error) {
+      console.error("Error fetching plans:", error);
+    }
+  },
+  async saveProduct({ rootState }, payload) {
+    try {
+      const franchiseId = rootState.franchises.idFranchise;
+      const db = this.$fire.firestore;
+
+      const product = await db
+        .collection("users")
+        .doc(rootState.auth.user.uid)
+        .collection("franchises")
+        .doc(franchiseId)
+        .collection("products")
+        .add(payload);
+
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export const getters = {
+  productsData: (state) => {
+    return state.products;
+  },
+};
