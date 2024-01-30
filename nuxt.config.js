997c32990971e189bb0d730aed5f69ebc3f7cf07
droid-janderson <jandersongit@gmail.com>
2024-01-30 01:21:13 -0300

    icon system

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,13 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/Smart Stock Logo - icon.png",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
